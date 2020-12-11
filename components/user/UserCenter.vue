ac91131592448e77e0cdf5639316094a2901440f
bieyi666 <1976251439@qq.com>
2020-12-11 09:12:19 +0800

    测试

--- components/user/UserCenter.vue
+++ components/user/UserCenter.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
     这里是用户中心
+    <router-link :to="url">
+      测试商户首页
+    </router-link>
   </div>
 </template>
 
 <script>
-    export default {
-        name: "UserCenter"
+  export default {
+    name: "UserCenter",
+    data() {
+      return {
+        url:'/merchant'
+      }
     }
+
+  }
 </script>
 
 <style scoped>
